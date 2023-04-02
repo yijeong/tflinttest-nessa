b9dc45e76342acfc78271bd9dbde292d2d138f66
nessa.829 <nessa.829@kakaostyle.com>
2023-04-03 00:57:48 +0900

    chore : lint 추가

--- awsresource/module/service/service.tf
+++ awsresource/module/service/service.tf
@@ -1,7 +1,7 @@
 # iterator
 resource "aws_ecr_repository" "this" {
   for_each    = toset(var.service_name)
-  name                 = "${each.value}-eks"
+  name                 = each.value
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = false
